9e7c170a48b593541934dcc876d0a42cab85f54c
sleyzerzon <senya22@yahoo.com>
2016-08-30 18:29:04 -0400

    handling the web service key

--- src/clj_financials/core.clj
+++ src/clj_financials/core.clj
@@ -10,10 +10,4 @@
 
 
 (client/get "https://services.last10k.com/v1/company/slw/quote"
-            {:headers {"Ocp-Apim-Subscription-Key" "baf26a417a8249e9a0e9dfc331e486c8"}})
-
-;(def ocp-apim-subscription-key
-;  (env :ocp-apim-subscription-key))
-
-(client/get "https://services.last10k.com/v1/company/slw/quote"
             {:headers {"Ocp-Apim-Subscription-Key" (env :ocp-apim-subscription-key)}})
